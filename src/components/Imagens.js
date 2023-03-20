b7d3560ed60dac72ebc58b9c6ff8620f17d41a53
joaogabriel <zala4453523@gmail.com>
2023-03-20 15:06:56 -0300

    V2

--- src/components/Imagens.js
+++ src/components/Imagens.js
@@ -3,17 +3,14 @@ import { View, Text, StyleSheet, Image, FlatList } from 'react-native';
 
 export default function Imagens() {
     const imagens = [
-        {caminho : '../imagens/capivara1.jpeg', nome:'capivara1'},
-        {caminho : '../imagens/capivara2.jpg', nome:'capivara2'},
-        {caminho : '../imagens/capivara3.jpeg', nome: 'capivara3'},
+        {caminho : 'https://preview.redd.it/dvr6gpwr26g91.jpg?width=960&crop=smart&auto=webp&v=enabled&s=e97a95eb86d324694e6271c2ba9d1054855fc0f3', nome:'capivara1'},
+        {caminho : 'https://img.ifunny.co/images/1388a672146de0b868da49ded6419b5991ef21897499ee156497071dee1f5012_1.jpg', nome:'capivara2'},
+        {caminho : 'https://imagens.ne10.uol.com.br/veiculos/_midias/jpg/2022/01/05/capivaras-20400556.jpg', nome: 'capivara3'},
     ]
     return (
         <View style={styles.container}>
             <Text>Imagens</Text>
-            {/* <View><Text style={styles.texto}>Capivara Tangerina</Text><Image style={styles.imagem} source={require('../imagens/capivara1.jpeg')} /></View>
-            <View><Text style={styles.texto}>Capivara Feliz</Text><Image style={styles.imagem} source={require('../imagens/capivara2.jpg')} /></View>
-            <View><Text style={styles.texto}>Capivara Pull Up</Text><Image style={styles.imagem} source={require('../imagens/capivara3.jpeg')} /></View> */}
-            <FlatList data={imagens} renderItem={({item}) => (<View><Text style={styles.texto}>{item.nome}</Text><Image style={styles.imagem} source={require('../imagens/capivara1.jpeg')} /></View>)}  />
+            <FlatList data={imagens} renderItem={({item}) => (<View><Text style={styles.texto}>{item.nome}</Text><Image style={styles.imagem} source={{uri: item.caminho}} /></View>)}  /> 
         </View>
     )
 }
